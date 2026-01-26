2d7270d1efd90c9aaa23bf0f398e3ffe4f8440e7
joepattrn <joe.green@pattrnstudios.com>
2026-01-26 21:56:46 +0000

    chore: Force redeploy

--- gmo-builder/api/pptx-test-a.js
+++ gmo-builder/api/pptx-test-a.js
@@ -1,6 +1,7 @@
 /**
  * Test A: Bare minimum handler - no imports
  * Tests if basic Vercel function works
+ * v2 - forced redeploy
  */
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
